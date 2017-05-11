616c0908b1a10790db7b59cae37cca93f6b0f881
aclarke <al216364@dal.ca>
2017-05-10 22:43:27 -0300

    Text box and submit button added and integrated

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,31 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    Button b;
+    EditText editText;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button);
+        editText= (EditText) findViewById(R.id.edittext);
+        text = (TextView) findViewById(R.id.helloText);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                text.setText(editText.getText().toString());
+            }
+        });
+
     }
 }
